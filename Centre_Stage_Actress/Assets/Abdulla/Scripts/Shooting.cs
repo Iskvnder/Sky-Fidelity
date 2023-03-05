2ddf2ac9a08e57e014bc430bade57177916c931e
ManStr1 <a.atentaev@mail.ru>
2023-03-05 17:31:38 +0600

    Shooting script restructuring

--- Centre_Stage_Actress/Assets/Abdulla/Scripts/Shooting.cs
+++ Centre_Stage_Actress/Assets/Abdulla/Scripts/Shooting.cs
@@ -10,60 +10,49 @@ public class Shooting : MonoBehaviour {
     private GameObject debrisPrefab;
 
     private Camera cam;
-    private float bulletImpulse = 10f;
     private float cooldown = 0.2f;
     private float cooldownRemaining = 0;
     private float range = 100f;
 
+    private Ray cameraTargetRay;
+    private RaycastHit cameraHitInfo;
+    private GameObject cameraHitObject;
+    private Vector3 cameraHitPoint = Vector3.zero;
+
+    private Ray playerTargetRay;
+    private RaycastHit playerHitInfo;
+    private GameObject playerHitObject;
+    private Vector3 playerHitPoint = Vector3.zero;
 
     void Start() {
         cam = Camera.main;
     }
 
-
     void Update() {
         cooldownRemaining -= Time.deltaTime;
-
-        //if (Input.GetButtonDown("Fire1")) {
-        //    GameObject bullet = Instantiate(bulletPrefab, cam.transform.position + cam.transform.forward * 0.4f, cam.transform.rotation);
-        //    bullet.GetComponent<Rigidbody>().AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse);
-        //}
 
         if (Input.GetButton("Fire1") && cooldownRemaining <= 0) {
             cooldownRemaining = cooldown;
 
-            Ray cameraTargetRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            RaycastHit cameraHitInfo;
-            GameObject cameraTargetObject;
-            Vector3 cameraHitPoint = Vector3.zero;
+            cameraTargetRay = new Ray(cam.transform.position, cam.transform.forward); // Ray path
 
-            if (Physics.Raycast(cameraTargetRay, out cameraHitInfo, range)) {
+            if (Physics.Raycast(cameraTargetRay, out cameraHitInfo, range)) { // raycast ray in this range, save info
                 cameraHitPoint = cameraHitInfo.point;
-                cameraTargetObject = cameraHitInfo.collider.gameObject;
-
-                Debug.Log("Hit object: " + cameraTargetObject.name);
-                Debug.Log("Hit point: " + cameraHitPoint);
+                cameraHitObject = cameraHitInfo.collider.gameObject;
             }
             
-
-            Ray playerTargetRay = new Ray(gameObject.transform.position, cameraHitInfo.point - gameObject.transform.position);
-            RaycastHit playerHitInfo;
-            GameObject playerTargetObject;
-            Vector3 playerHitPoint = Vector3.zero;
-
-            if (Physics.Raycast(playerTargetRay, out playerHitInfo, range)) {
+            playerTargetRay = new Ray(gameObject.transform.position, cameraHitInfo.point - gameObject.transform.position); // Ray second path
+            
+            if (Physics.Raycast(playerTargetRay, out playerHitInfo, range)) { // raycast ray in this range, save info
                 playerHitPoint = playerHitInfo.point;
-                playerTargetObject = playerHitInfo.collider.gameObject;
-
-                Debug.Log("Hit object2: " + playerTargetObject.name);
-                Debug.Log("Hit point2: " + playerHitPoint);
+                playerHitObject = playerHitInfo.collider.gameObject;
             }
 
             if (cameraHitPoint == playerHitPoint) {
                 Debug.Log("I can hit!");
             }
 
-            if (debrisPrefab != null) {
+            if (debrisPrefab != null) { // Shooting Particle
                 Instantiate(debrisPrefab, playerHitPoint, Quaternion.identity);
             }
         }
