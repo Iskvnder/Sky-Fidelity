9001f0b12dcab9f2eb1c43942c7ceb04d32e4a99
Iskandar Rassulov <iskanderesz@gmail.com>
2023-03-10 20:56:32 +0600

    Walking Aim Animation
    - no running
    - no jumping
    - no backward anim with gun

--- SkyFi/Assets/Iskander/Scripts/AnimController.cs
+++ SkyFi/Assets/Iskander/Scripts/AnimController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class AnimController : MonoBehaviour
 {
     private Animator playerAnimator;
-    private string[] animationsArray = {"isWalkF", "isWalkB", "isWalkL", "isWalkR", "isWalkFL", "isWalkFR", "isWalkBL", "isWalkBR"};
+    private string[] animationsArray = {"isWalkF", "isWalkB", "isWalkL", "isWalkR", 
+    "isWalkFL", "isWalkFR", "isWalkBL", "isWalkBR", "isAim", "isAimF", "isAimL", "isAimR"};
+
+    private bool battleOn = false;
 
     void Start()
     {
@@ -20,6 +23,7 @@ public class AnimController : MonoBehaviour
     }
 
     void animPlay(){
+
         bool forwardPress = Input.GetKey(KeyCode.W);
         bool backwardPress = Input.GetKey(KeyCode.S);
         bool leftPress = Input.GetKey(KeyCode.A);
@@ -29,29 +33,53 @@ public class AnimController : MonoBehaviour
         bool backwardLeftPress = (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A));
         bool backwardRightPress = (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D));
 
-        if(forwardPress){setAnimation(0);}
-        if(!forwardPress){resetAnimation(0);}
+        
+        bool rmbPress = Input.GetKey(KeyCode.Mouse1);
+        bool rmbForwardPress = (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Mouse1));
+        bool rmbLeftPress = (Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.A));
+        bool rmbRightPress = (Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.D));
 
-        if(backwardPress){setAnimation(1);}
-        if(!backwardPress){resetAnimation(1);}
 
-        if(leftPress){setAnimation(2);}
-        if(!leftPress){resetAnimation(2);}
+            if(forwardPress){setAnimation(0);}
+            if(!forwardPress){resetAnimation(0);}
 
-        if(rightPress){setAnimation(3);}
-        if(!rightPress){resetAnimation(3);}
+            if(backwardPress){setAnimation(1);}
+            if(!backwardPress){resetAnimation(1);}
 
-        if(forwardLeftPress){setAnimation(4); resetAnimation(0); resetAnimation(2);}
-        if(!forwardLeftPress){resetAnimation(4);}
+            if(leftPress){setAnimation(2);}
+            if(!leftPress){resetAnimation(2);}
 
-        if(forwardRightPress){setAnimation(5); resetAnimation(0); resetAnimation(3);}
-        if(!forwardRightPress){resetAnimation(5);}
+            if(rightPress){setAnimation(3);}
+            if(!rightPress){resetAnimation(3);}
 
-        if(backwardLeftPress){setAnimation(6); resetAnimation(1); resetAnimation(2);}
-        if(!backwardLeftPress){resetAnimation(6);}
+            if(forwardLeftPress){setAnimation(4); resetAnimation(0); resetAnimation(2);}
+            if(!forwardLeftPress){resetAnimation(4);}
 
-        if(backwardRightPress){setAnimation(7); resetAnimation(1); resetAnimation(3);}
-        if(!backwardRightPress){resetAnimation(7);}
+            if(forwardRightPress){setAnimation(5); resetAnimation(0); resetAnimation(3);}
+            if(!forwardRightPress){resetAnimation(5);}
+
+            if(backwardLeftPress){setAnimation(6); resetAnimation(1); resetAnimation(2);}
+            if(!backwardLeftPress){resetAnimation(6);}
+
+            if(backwardRightPress){setAnimation(7); resetAnimation(1); resetAnimation(3);}
+            if(!backwardRightPress){resetAnimation(7);}
+
+        
+
+            if(rmbPress){setAnimation(8);}
+            if(!rmbPress){resetAnimation(8);}
+
+       
+            if(rmbForwardPress){setAnimation(9); resetAnimation(0);}
+            if(!rmbForwardPress){resetAnimation(9);}
+
+            if(rmbLeftPress){setAnimation(10); resetAnimation(2);}
+            if(!rmbLeftPress){resetAnimation(10);}
+
+            if(rmbRightPress){setAnimation(11); resetAnimation(3);}
+            if(!rmbRightPress){resetAnimation(11);}
+        
+
     }
 
     void setAnimation(int num){
